4c11c5acd7990d4ed07c26c4b40183510354625f
mubashir-cvr <mubashircheruvayoor@gmail.com>
2022-01-27 16:33:05 +0530

    React Element

--- src/index.js
+++ src/index.js
@@ -1,7 +1,12 @@
 import React from "react"
 import ReactDOM from "react-dom"
-function Greetings(){
-  return <h1>Hi From React Component</h1>;
+// function Greetings(){
+//   return <h4>Hi React</h4>;
+// }
+
+const  Greetings =()=> {
+  return React.createElement('div',{},React.createElement('h1',{},'Hi'))
+  
 }
 
 ReactDOM.render(<Greetings/>,document.getElementById("root"))
