2c5cab23ce13f3c59697168d991a03cc547efaa2
mubashir-cvr <mubashircheruvayoor@gmail.com>
2022-01-27 18:49:55 +0530

    Adding inherited component

--- src/index.js
+++ src/index.js
@@ -1,12 +1,28 @@
 import React from "react"
 import ReactDOM from "react-dom"
-// function Greetings(){
-//   return <h4>Hi React</h4>;
-// }
-
-const  Greetings =()=> {
-  return React.createElement('div',{},React.createElement('h1',{},'Hi'))
+function BookList(){
   
+  return (
+  <section>
+    <Book/>
+    
+    
+  </section>
+  );
 }
 
-ReactDOM.render(<Greetings/>,document.getElementById("root"))
+const Book=() =>{
+  return <div>
+  <h4>My book</h4>
+  <Image/>
+  </div>
+  }
+const Image=() =>{return <img width={100} src="https://images.unsplash.com/photo-1543002588-bfa74002ed7e?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxleHBsb3JlLWZlZWR8MXx8fGVufDB8fHx8&w=1000&q=80" />}
+const Message=() =><p>My Message</p>
+
+// const  BookList =()=> {
+//   return React.createElement('div',{},React.createElement('h1',{},'Hi'))
+  
+// }
+
+ReactDOM.render(<BookList/>,document.getElementById("root"))
