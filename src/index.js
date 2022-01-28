22a72a16b9425327cfc814b0f16ec6b49d74f30c
mubashir-cvr <mubashircheruvayoor@gmail.com>
2022-01-28 09:46:47 +0530

    Add Key value (Unique Value )

--- src/index.js
+++ src/index.js
@@ -3,34 +3,41 @@ import ReactDOM from "react-dom"
 
 import './index.css'
 
-const firtsbook={
+const books=[{
+  id:1,
+  img:"https://dummyimage.com/150x200.gif?text=book",
   title:"My Title from dict",
+  author:"Author"
 
-}
+},
+{
+  id:2,
+  img:"https://dummyimage.com/150x200.gif?text=book",
+
+  title:"My Title from dict",
+  author:"Author"
+
+
+}]
 function BookList(){
   
   return (
   <section className="booklist">
-    <Book  title={firtsbook.title} h="Next Param">
-      <p>Lorem, ipsum dolor sit amet consectetur adipisicing elit. Eaque explicabo ducimus, fuga, porro sed deserunt est alias dolor dicta deleniti fugit, aspernatur quis blanditiis amet debitis reprehenderit itaque quia! Labore.</p>
-    </Book>
-    <Book title="My Title1" h="Next Param"/>
-    <Book title="My Title2" h="Next Param"/>
-    <Book title="My Title3" h="Next Param"/>
-    
-    
+    {books.map((book,index)=>{
+      const {img,title,author}=book
+      return <Book key ={book.id} book={book}></Book>
+    })}
   </section>
   );
 }
 
-const Book=({title,h,children}) =>{
-  
+const Book=(props) =>{
+  const {img,title,auth} = props.book
   return <section className="book">
   
   <Image/>
   <h2>{title}</h2>
-  <p>{h}</p>
-  {children}
+  <p>{auth}</p>
   </section>
   }
 const Image=() =>{return <img src="https://dummyimage.com/150x200.gif?text=book" />}
